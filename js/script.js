f930384595926c7ca9441f6bd3bb77e7f65801f5
cyrus <cyrus@coderepubliq.com>
2015-08-26 12:49:22 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function($) {
+
+    $(window).resize(function(){
+       $('.height-follower, .height-follower-mobile').height($(window).height());
+    })
+    $(window).resize();
+
+	/*$('a').smoothScroll();*/
+
+    $(function() {
+        $('header a').bind('click',function(event){
+            var $anchor = $(this);
+     
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500,'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $('.conceptology-retail-carousel').owlCarousel({
+        items: 1,
+        navContainer: "#portfolio-retail-nav",
+        dotsContainer: "#portfolio-retail-dots"
+    });
+    if ($(window).width() <= 768) {
+            $('body').addClass('height-follower-mobile');
+            $('.social-links-container').insertAfter('.mail-input-container');
+        }else{
+            $('.social-links-container').insertAfter('.zgenius-text-logo');
+        }
+    $(window).resize(function(){
+        if ($(window).width() <= 768) {
+            $('body').addClass('height-follower-mobile');
+            $('.social-links-container').insertAfter('.mail-input-container');
+        }else{
+            $('.social-links-container').insertAfter('.zgenius-text-logo');
+        }
+    });
+
+});
